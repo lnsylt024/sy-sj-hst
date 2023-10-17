bb41eba6bc38bb3f52d8d8178b12f7b678fc44a2
lnsylt <lnsylt@163.com>
2023-10-17 18:03:08 +0800

    update first

--- src/main/java/com/example/sysjhst/SySjHstApplication.java
+++ src/main/java/com/example/sysjhst/SySjHstApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SySjHstApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello, My Application!");
 		SpringApplication.run(SySjHstApplication.class, args);
 	}
 
