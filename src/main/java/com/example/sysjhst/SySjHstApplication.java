d25f63a588c2557b25dadad8351d80f4cc3baa90
lnsylt <lnsylt@163.com>
2023-10-17 17:51:33 +0800

    Initial commit

---
+++ src/main/java/com/example/sysjhst/SySjHstApplication.java
@@ -0,0 +1,13 @@
+package com.example.sysjhst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SySjHstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SySjHstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sysjhst/SySjHstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sysjhst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SySjHstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
